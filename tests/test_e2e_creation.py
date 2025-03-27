0f8e562a4024d4518c42f5a9308db7c2d5e48aa0
Matthias Samland <m.samland@mailbox.org>
2025-03-27 12:23:41 +0100

    End-to-end testing. Fixes #4

---
+++ tests/test_e2e_creation.py
@@ -0,0 +1,4 @@
+def test_database_creation(persistent_master_table, persistent_target_table, persistent_observation_table):
+    assert len(persistent_master_table) > 0
+    assert len(persistent_target_table) > 0
+    assert len(persistent_observation_table) > 0
