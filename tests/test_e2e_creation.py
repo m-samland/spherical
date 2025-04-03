1d33833501bf116d3b34adf4a5041c9b1d7c4c75
Matthias Samland <m.samland@mailbox.org>
2025-04-03 10:37:06 +0200

    Add tests for some entries in observation table

--- tests/test_e2e_creation.py
+++ tests/test_e2e_creation.py
@@ -1,4 +1,9 @@
+import numpy as np
+
+
 def test_database_creation(persistent_master_table, persistent_target_table, persistent_observation_table):
     assert len(persistent_master_table) > 0
     assert len(persistent_target_table) > 0
     assert len(persistent_observation_table) > 0
+    assert np.all(persistent_observation_table['TOTAL_EXPTIME'] > 0)
+    assert np.all(persistent_observation_table['ROTATION'] >= 0)
